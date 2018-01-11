97a68e5202d6500d1359916780e6e6e2b4c76960
Niklas Hietaranta <K9279@Labranet.jamk.fi>
2018-01-11 16:51:44 +0200

    Add project files.

---
+++ tehtavat1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace tehtavat1
+{
+    class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+
+            Console.Write("Anna numero > ");
+            int number = int.Parse(Console.ReadLine());
+            switch (number)
+            {
+                case 1: Console.WriteLine("numero on yksi"); break;
+                case 2: Console.WriteLine("numero on kaksi"); break;
+                case 3: Console.WriteLine("numero on kolme"); break;
+
+            }
+            if (number > 3)
+            {
+                Console.WriteLine("Joku muu luku");
+            }
+            if (number < 1)
+            {
+                Console.WriteLine("joku muu luku");
+            }
+
+        }
+    }
+}
---
+++ tehtavat2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+
+namespace tehtavat1
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+            Console.Write("Give point limit > ");
+            int number = int.Parse(Console.ReadLine());
+            if (number ==0 || number ==1 )
+            {
+                Console.WriteLine("School number is 0");
+            }
+            if (number ==2 || number ==3)
+            {
+                Console.WriteLine("School number is 1");
+            }
+            if (number == 4 || number == 5)
+            {
+                Console.WriteLine("School number is 2");
+            }
+            if (number == 6 || number == 7)
+            {
+                Console.WriteLine("School number is 3");
+            }
+            if (number == 8 || number == 9)
+            {
+                Console.WriteLine("School number is 4");
+            }
+            if (number == 10 || number == 11 || number ==12)
+            {
+                Console.WriteLine("School number is 5");
+            }
+            if (number > 12)
+            {
+                Console.WriteLine("et osaa");
+            }
+            if (number < 0)
+            {
+                Console.WriteLine("et osaa");
+            }
+        }
+    }
+}
