02dfe226baa0b27b3a48be9a890f47750dc99ccd
Niklas Hietaranta <K9279@student.jamk.fi>
2018-01-15 05:14:09 +0200

    teht9

---
+++ tehtava9/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tehtava9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int heh = 1;
+            //int yht = 0;
+            int perse = 0;
+            while (heh != 0) {
+                Console.Write("anna kokonais lukuja. ");
+                heh = int.Parse(Console.ReadLine());
+                perse = (perse + heh);
+                //perse + (yht + heh);
+                //Console.Write(heh);
+                //Console.Write(perse);
+                //Console.Write(yht);
+
+            }
+         
+            Console.Write("lukujen summa on: ");
+            Console.Write(perse);
+            Console.Write("\n");
+        }
+    }
+}
