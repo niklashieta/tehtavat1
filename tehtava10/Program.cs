592799559a2d927841df64bea3d52cdd14d36933
Niklas Hietaranta <K9279@student.jamk.fi>
2018-01-15 05:29:32 +0200

    teht10

---
+++ tehtava10/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tehtava10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbers;
+            numbers = new[9];
+            numbers[0] = 1;
+            numbers[1] = 2;
+            numbers[2] = 33;
+            numbers[3] = 44;
+            numbers[4] = 55;
+            numbers[5] = 68;
+            numbers[6] = 77;
+            numbers[7] = 96;
+            numbers[8] = 100;
+        }
+    }
+}
