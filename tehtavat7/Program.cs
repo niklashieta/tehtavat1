21de438633f1e16e9dd7d6c1366b5a8c67f634c1
Niklas Hietaranta <K9279@student.jamk.fi>
2018-01-15 04:12:37 +0200

    tehtavaa7

---
+++ tehtavat7/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tehtavat7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Anna vuosi:");
+            int vuosi = int.Parse(Console.ReadLine());
+            if (vuosi % 4 == 0)
+            {
+                if (vuosi % 100 == 0)
+                {
+                    if (vuosi % 400 == 0)
+                    {
+                        Console.Write("On karkausvuosi.");
+                    }
+                    else
+                    {
+                        Console.Write("Ei ole karkausvuosi.");
+                    }
+                }
+                else
+                {
+                    Console.Write("On karkausvuosi.");
+                }
+            }
+            else
+            {
+                Console.Write("Ei ole karkausvuosi.");
+            }
+        }
+    }
+}
