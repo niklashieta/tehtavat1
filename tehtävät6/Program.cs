4122956a637397ed772373242586ba40693a9e49
Niklas Hietaranta <K9279@student.jamk.fi>
2018-01-15 03:32:02 +0200

    teht7

---
+++ tehtävät6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tehtävät6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Montako kilometriä ajoit?: ");
+            double dist = double.Parse(Console.ReadLine());
+            double kulutus = 7.02;
+            double bensa = 1.595;
+            double matka = dist / 100 * kulutus;
+            double hinta = matka * bensa;
+            Console.Write("ajoit ");
+            Console.Write(dist);
+            Console.Write("Km auto kulutti ");
+            Console.Write(matka);
+            Console.Write("Litraa. Rahaa bensaan kului ");
+            Console.Write(hinta);
+            Console.Write("Euroa\n");
+        }
+    }
+}
